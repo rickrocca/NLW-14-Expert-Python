cd2354892f96a32850359c7dea4b2410cf93d5e4
ZombZombs <henriquerocca@gmail.com>
2024-02-06 20:38:44 -0300

    feat: Implementing View for tag creator with HTTP Types

---
+++ src/views/tag_creator_view.py
@@ -0,0 +1,15 @@
+from src.views.http_types.http_request import HttpRequest
+from src.views.http_types.http_response import HttpResponse
+
+class TagCreatorView:
+    '''
+        Responsability for interacting with HTTP
+    '''
+    def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        #body = http_request.body
+        #product_code = body["product_code"]
+
+        #logica de regra de negocio
+        print(http_request)
+        #retorno HTTP
+        return HttpResponse(status_code=200, body={"hello": "world"})
