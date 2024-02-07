a9bcf789beab530052e4663d364dcc8cb2e06da9
ZombZombs <henriquerocca@gmail.com>
2024-02-07 20:37:01 -0300

    feat: Implementing Validator

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,6 +1,21 @@
 from src.views.http_types.http_response import HttpResponse
+from .error_types.http_unprocessable_entity import HttpUnprocessableEntityError
 
 def handle_error(error: Exception) -> HttpResponse:
+    if isinstance(error, HttpUnprocessableEntityError):
+        return HttpResponse(
+            #enviar um log
+            #enviar um Email
+            status_code=error.status_code,
+            body= {
+                "errors": [{
+                    "title": error.name,
+                    "details": error.message
+                }]
+            }
+        )
+
+
     return HttpResponse(
         status_code=500,
         body={
